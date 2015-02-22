3e87dfc7057d92dcc9544c800eb9c2cf974d24b6
Adam Burton <adam@burt0n.net>
2015-02-22 12:34:36 +0000

    Remove pliant stuff

--- articulate/articulate.lua
+++ articulate/articulate.lua
@@ -18,37 +18,6 @@ local rawset = rawset
 
 module('articulate')
 
-local drivers = {}
-local connections = {}
-
-function AvailableDrivers()
-	return drivers
-end
-
-function RegisterDriver(name, className)
-	drivers[name] = className
-end
-
-function Connection(driver, connectionInfo)
-	assert(drivers[driver], driver .. ' must be a registered driver')
-	
-	try (function()
-		return new (drivers[name], unpack(connectionInfo))
-	end)
-	catch ({
-		['ArticulateMissingDriverException'] = function(err)
-			
-		end,
-		['ArticulateConnectionErrorException'] = function(err)
-			
-		end
-	})
-end
-
-function QueryBuilder(connection)
-	return new ('ArticulateQueryBuilder', connection)
-end
-
 function ModelQueryBuilder(connection, model)
 	return new ('ArticulateModelQueryBuilder', connection):setModel(model)
 end
